1c079143bfdb02e24b7c5f35aea72ff387665131
RomainLCBT <romain.lecaillon@outlook.com>
2026-03-09 10:27:47 +0100

    je suis un animal

--- src/animal.cpp
+++ src/animal.cpp
@@ -8,15 +8,36 @@ Animal::Animal(int maxX, int maxY)
 Animal::Animal(int maxX, int maxY, int a, int b)
 {
 
+    if(a >= 0 && a < maxX) {
+        this->x = a;
+    } else {
+        this->x = 0; 
+    }
+
+    if(b >= 0 && b < maxY) {
+        this->y = b;
+    } else {
+        this->y = 0;
+    }
+
+    this->vivant = 1;
 }
 
 
-void Animal::setVivant(bool v)
-{
 
+void Animal::setVivant(bool v){
+    this->vivant = v;
 }
 
-bool Animal::attaque (Animal &a)
-{
-    
+
+bool Animal::attaque(Animal &a){
+    if (this->vivant && a.vivant) {
+ 
+        return true; 
+    }
+    return false;
 }
+
+
+
+
